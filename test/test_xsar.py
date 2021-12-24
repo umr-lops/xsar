cf4b06f9445e8ed6467173c33bd402663a0a79e9
Alexandre Levieux <alexandre.levieux@gmail.com>
2021-12-24 13:28:30 +0100

    add pytest framework

--- test/test_xsar.py
+++ test/test_xsar.py
@@ -2,6 +2,8 @@ import xsar
 import rasterio
 import os
 import logging
+import pytest
+import pickle
 
 logging.basicConfig()
 logging.getLogger('xsar').setLevel(logging.DEBUG)
@@ -12,15 +14,24 @@ logging.captureWarnings(True)
 logger = logging.getLogger('xsar_test')
 logger.setLevel(logging.DEBUG)
 
-
 # logger.info('using %s as test_dir' % xsar.config['data_dir'])
 
 
 meta = xsar.Sentinel1Meta(
     xsar.get_test_file('S1A_IW_GRDH_1SDV_20170907T103020_20170907T103045_018268_01EB76_Z010.SAFE'))
-ds = xsar.open_dataset(meta, resolution={'atrack': 100, 'xtrack': 100}, resampling=rasterio.enums.Resampling.average)
 
-ds.compute()
 
-ds = xsar.open_dataset(meta).isel(pol=0,atrack=slice(0,100),xtrack=slice(0,100))
-ds.compute()
+def test_open_dataset():
+    try:
+        ds = xsar.open_dataset(meta, resolution={'atrack': 100, 'xtrack': 100}, resampling=rasterio.enums.Resampling.average)
+        ds.compute()
+        ds = xsar.open_dataset(meta).isel(pol=0, atrack=slice(0, 100), xtrack=slice(0, 100))
+        ds.compute()
+        assert True
+    except:
+        assert False
+
+
+# def test_serializable_s1_meta():
+#     s1meta = pickle.loads(pickle.dumps(meta))
+#     assert isinstance(tuple, s1meta.coords2ll(100, 100))

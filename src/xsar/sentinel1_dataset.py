umr-lops/xsar

commit 37d2ee4b5ff8d88e81e8fe2d8bdb787dd6f06107

allow string in 'resolution' kw like '100m'
also works for intermediate resolution: 100m resolution is possible with EW_GRDM with a pixel size of 40m

src/xsar/sentinel1_dataset.py
@@ -34,17 +34,32 @@ class Sentinel1Dataset:
     Parameters
     ----------
     dataset_id: str or Sentinel1Meta object
+
         if str, it can be a path, or a gdal dataset identifier like `'SENTINEL1_DS:%s:WV_001' % filename`)
-    resolution: dict, optional
+
+    resolution: dict, number or string, optional
         resampling dict like `{'atrack': 20, 'xtrack': 20}` where 20 is in pixels.
+
+        if a number, dict will be constructed from `{'atrack': number, 'xtrack': number}`
+
+        if str, it must end with 'm' (meters), like '100m'. dict will be computed from sensor pixel size.
+
     resampling: rasterio.enums.Resampling or str, optional
+
         Only used if `resolution` is not None.
+
         ` rasterio.enums.Resampling.rms` by default. `rasterio.enums.Resampling.nearest` (decimation) is fastest.
+
     luts: bool, optional
+
         if `True` return also luts as variables (ie `sigma0_lut`, `gamma0_lut`, etc...). False by default.
+
     chunks: dict, optional
+
         dict with keys ['pol','atrack','xtrack'] (dask chunks).
+
     dtypes: None or dict, optional
+
         Specify the data type for each variable.
 
     See Also
@@ -264,6 +279,7 @@ class Sentinel1Dataset:
         Parameters
         ----------
         *args: lon, lat or shapely object
+
             lon and lat might be iterables or scalars
 
         Returns
@@ -344,7 +360,7 @@ class Sentinel1Dataset:
 
     @property
     def _regularly_spaced(self):
-        return max([np.unique(np.diff(self._dataset[dim].values)).size for dim in ['atrack', 'xtrack']]) == 1
+        return max([np.unique(np.round(np.diff(self._dataset[dim].values),1)).size for dim in ['atrack', 'xtrack']]) == 1
 
     def _recompute_attrs(self):
         if not self._regularly_spaced:
@@ -420,7 +436,7 @@ class Sentinel1Dataset:
 
         Parameters
         ----------
-        resolution: None or dict
+        resolution: None, number, str or dict
             see `xsar.open_dataset`
         resampling: rasterio.enums.Resampling
             see `xsar.open_dataset`
@@ -464,17 +480,27 @@ class Sentinel1Dataset:
             # 0.5 is for pixel center (geotiff standard)
             dn = dn.assign_coords({'atrack': dn.atrack + 0.5, 'xtrack': dn.xtrack + 0.5})
         else:
+            if not isinstance(resolution, dict):
+                if isinstance(resolution, str) and resolution.endswith('m'):
+                    resolution = float(resolution[:-1])
+                resolution = dict(atrack=resolution / self.s1meta.pixel_atrack_m, xtrack=resolution / self.s1meta.pixel_xtrack_m)
+
             # resample the DN at gdal level, before feeding it to the dataset
             out_shape = (
-                rio.height // resolution['atrack'],
-                rio.width // resolution['xtrack']
+                int(rio.height / resolution['atrack']),
+                int(rio.width / resolution['xtrack'])
             )
             out_shape_pol = (1,) + out_shape
             # read resampled array in one chunk, and rechunk
             # this doesn't optimize memory, but total size remain quite small
-            # winsize is the maximum full image size that can be divided  by resolution (int)
-            winsize = (0, 0, rio.width // resolution['xtrack'] * resolution['xtrack'],
-                       rio.height // resolution['atrack'] * resolution['atrack'])
+
+            if isinstance(resolution['atrack'], int):
+                # legacy behaviour: winsize is the maximum full image size that can be divided  by resolution (int)
+                winsize = (0, 0, rio.width // resolution['xtrack'] * resolution['xtrack'],
+                           rio.height // resolution['atrack'] * resolution['atrack'])
+                window = rasterio.windows.Window(*winsize)
+            else:
+                window = None
 
             dn = xr.concat(
                 [
@@ -483,7 +509,7 @@ class Sentinel1Dataset:
                             rasterio.open(f).read(
                                 out_shape=out_shape_pol,
                                 resampling=resampling,
-                                window=rasterio.windows.Window(*winsize)
+                                window=window
                             ),
                             chunks=chunks_rio
                         ),

docs/examples/xsar_advanced.ipynb
@@ -90,17 +90,13 @@
    "metadata": {},
    "source": [
     "## Open a dataset at lower resolution\n",
-    "We can use `sar_meta` properties to compute the pixel count we need to average to get a resolution of 200*200 meters"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "resolution = {'atrack' : int(np.round(200 / sar_meta.pixel_atrack_m)), 'xtrack': int(np.round(200 / sar_meta.pixel_xtrack_m))}\n",
-    "resolution"
+    "\n",
+    "`resolution` keyword can be used to open a dataset at lower resolution. \n",
+    "\n",
+    "It might be:\n",
+    "\n",
+    " * a dict `{'atrack': 20, 'xtrack': 20}`: 20*20 pixels. so if sensor resolution is 10m, the final resolution will be 100m\n",
+    " * a string like `'200m'`: Sensor resolution will be automatically used to compute the window size"
    ]
   },
   {
@@ -116,7 +112,7 @@
    "metadata": {},
    "outputs": [],
    "source": [
-    "sar_ds = xsar.Sentinel1Dataset(sar_meta, resolution=resolution)\n",
+    "sar_ds = xsar.Sentinel1Dataset(sar_meta, resolution='200m')\n",
     "sar_ds"
    ]
   },

src/xsar/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ['open_dataset', 'product_info', 'Sentinel1Meta', 'Sentinel1Dataset', 'SentinelMeta', 'SentinelDataset']
+__all__ = ['open_dataset', 'product_info', 'Sentinel1Meta', 'Sentinel1Dataset']
 from .xsar import *
 from .xsar import __version__

commit 6f110c16e4908ddee21b0ae880bc90a69f766381

correct a bug introduced in 37d2ee4b5f (np.diff(axtrack) not strictly equal) and replaced transpose by Affine.permutation()

src/xsar/sentinel1_dataset.py
@@ -106,7 +106,7 @@ class Sentinel1Dataset:
             # check serializable
             #import pickle
             #s1meta = pickle.loads(pickle.dumps(self.s1meta))
-            #assert isinstance(tuple,s1meta.coords2ll(100, 100))
+            #assert isinstance(s1meta.coords2ll(100, 100),tuple)
         else:
             # we want self.s1meta to be a dask actor on a worker
             self.s1meta = BlockingActorProxy(Sentinel1Meta.from_dict, dataset_id.dict)
@@ -596,7 +596,7 @@ class Sentinel1Dataset:
             # chunk footprint polygon, in lon/lat
             chunk_footprint_ll = self.s1meta.coords2ll(chunk_footprint_coords)
 
-            # get vector mask over chunk
+            # get vector mask over chunk, in lon/lat
             vector_mask_ll = self.s1meta.get_mask(mask).intersection(chunk_footprint_ll)
 
             if vector_mask_ll.is_empty:
@@ -606,13 +606,22 @@ class Sentinel1Dataset:
             # vector mask, in atrack/xtrack coordinates
             vector_mask_coords = self.s1meta.ll2coords(vector_mask_ll)
 
+            # shape of the returned chunk
+            out_shape = (atrack.size, xtrack.size)
+
+            # transform * (x, y) -> (atrack, xtrack)
+            # (where (x, y) are index in out_shape)
+            # Affine.permutation() is used because (atrack, xtrack) is transposed from geographic
+
+            transform = Affine.translation(*chunk_coords[0]) * Affine.scale(
+                *[np.unique(np.diff(c))[0] for c in [atrack, xtrack]]) * Affine.permutation()
+
             raster_mask = rasterio.features.rasterize(
                 [vector_mask_coords],
-                out_shape=(xtrack.size, atrack.size),
+                out_shape=out_shape,
                 all_touched=False,
-                transform=Affine.translation(*chunk_coords[0]) * Affine.scale(
-                    *[np.unique(np.diff(c)).item() for c in [xtrack, atrack]])
-            ).T
+                transform=transform
+            )
             return raster_mask
 
         da_list = [
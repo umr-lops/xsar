34149ba0c75161bf31bbb1a187df22d514882eb8
grouazel <antoine.grouazel@ifremer.fr>
2022-03-08 22:39:18 +0100

    test to have meta like in dvelop

--- src/xsar/sentinel1_meta.py
+++ src/xsar/sentinel1_meta.py
@@ -952,10 +952,4 @@ class Sentinel1Meta:
         new.__dict__.update(minidict)
         return new
 
-    @property
-    def _doppler_estimate(self):
-        dce = self.xml_parser.get_compound_var(self.files['annotation'].iloc[0], 'doppler_estimate')
-        dce.attrs['history'] = self.xml_parser.get_compound_var(self.files['annotation'].iloc[0], 'doppler_estimate',describe=True)
-        return dce
-
   

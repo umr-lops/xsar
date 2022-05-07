85128377e0964d87f38341e5cb0bc5f6dd55f349
Olivier Archer <olivier.archer@ifremer.fr>
2022-05-08 01:29:27 +0200

    CI build-matrix addon fix

---
+++ .github/build_matrix.py
@@ -0,0 +1,5 @@
+import sys
+
+if __name__ == "__main__":
+    print('::set-output name=os_matrix::["ubuntu-latest", "macos-latest", "windows-latest"]')
+    print('::set-output name=python_version_matrix::["3.7", "3.8", "3.9", "3.10"]')

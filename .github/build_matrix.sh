e8812543f5337be57a7330cfffbb2e1139eb2351
Olivier Archer <olivier.archer@ifremer.fr>
2022-05-08 01:17:25 +0200

    CI build-matrix addon fix

--- .github/build_matrix.sh
+++ .github/build_matrix.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo '["ubuntu-latest", "macos-latest", "windows-latest"]'
+echo '::set-output name=os_matrix::["ubuntu-latest", "macos-latest", "windows-latest"]'
+echo '::set-output name=python_version_matrix::["3.7", "3.8", "3.9", "3.10"]'
+
 exit 0
-
-
 
 if [ "$1" == "os" ]; then
   echo '["ubuntu-latest", "macos-latest", "windows-latest"]'

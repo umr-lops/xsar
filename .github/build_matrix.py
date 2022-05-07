d528bc4a745d6a81ea8bb3b14314a4a2c317fd64
Olivier Archer <olivier.archer@ifremer.fr>
2022-05-08 01:46:43 +0200

    CI build-matrix addon fix

--- .github/build_matrix.py
+++ .github/build_matrix.py
@@ -1,7 +1,18 @@
 import sys
 
-if __name__ == "__main__":
+matrix = {
+    'default': {
+        'os': ['ubuntu-latest'],
+        'python_version': ['3.10']
+    }
 
-    print("event: %s" % sys.argv[1])
-    print('::set-output name=os_matrix::["ubuntu-latest", "macos-latest", "windows-latest"]')
-    print('::set-output name=python_version_matrix::["3.7", "3.8", "3.9", "3.10"]')
+}
+
+if __name__ == "__main__":
+    event = sys.argv[1]
+    if event not in matrix:
+        event = 'default'
+
+
+    print('::set-output name=os_matrix::%s' % str(matrix[event]['os']))
+    print('::set-output name=python_version_matrix::%s' % str(matrix[event]['python_version']))

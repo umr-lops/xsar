3643541a5f9c636e956b689d19ac375de01c3602
Olivier Archer <olivier.archer@ifremer.fr>
2022-05-08 02:08:58 +0200

    CI build-matrix addon fix

--- .github/build_matrix.py
+++ .github/build_matrix.py
@@ -1,10 +1,15 @@
 import sys
+# minimal python script that returns a strategy matrix, for given github.event_name
 
 matrix = {
     'default': {
         'os': ['ubuntu-latest'],
         'python_version': ['3.10']
-    }
+    },
+    'schedule': {
+        'os': ['ubuntu-latest', 'macos-latest', 'windows-latest'],
+        'python_version': ['3.7', '3.8', '3.9', '3.10']
+    },
 
 }
 

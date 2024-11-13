9bd478820b7ded3be443b0a43d5a4f4f0663fd72
grouazel <antoine.grouazel@ifremer.fr>
2024-11-13 12:21:48 +0100

    adapt install-upstream-dev.sh

--- ci/install-upstream-dev.sh
+++ ci/install-upstream-dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-conda remove -y --force cytoolz numpy xarray construct toolz fsspec python-dateutil pandas
+conda remove -y --force cytoolz numpy xarray toolz python-dateutil pandas
 python -m pip install \
   -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple \
   --no-deps \
@@ -10,8 +10,5 @@ python -m pip install \
   pandas \
   xarray
 python -m pip install --upgrade \
-  git+https://github.com/construct/construct \
   git+https://github.com/pytoolz/toolz \
-  git+https://github.com/xarray-contrib/datatree \
-  git+https://github.com/fsspec/filesystem_spec \
   git+https://github.com/dateutil/dateutil
